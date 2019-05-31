a6ccb5c2a7885075b9c9f513d306c3a520625f70
Ahrav Dutta <ahravdutta02@gmail.com>
2019-05-30 20:58:53 -0700

    add profile about component

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const ProfileAbout = ({
@@ -7,10 +7,30 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => {
-  return <div />;
-};
+}) => (
+  <div class='profile-about bg-light p-2'>
+    {bio && (
+      <Fragment>
+        <h2 class='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
+        <p>{bio}</p>
+      </Fragment>
+    )}
 
-ProfileAbout.propTypes = {};
+    <div class='line' />
+    <h2 class='text-primary'>Skill Set</h2>
+    <div class='skills'>
+      {skills.map((skill, index) => (
+        <div key={index} className='p-1'>
+          <i className='fas fa-check' />
+          {skill}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
