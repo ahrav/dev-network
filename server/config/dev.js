22f2e7457923a695a0e7c86e0a76bf120c77dd95
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-09 02:20:22 -0700

    update env variables

--- server/config/dev.js
+++ server/config/dev.js
@@ -4,6 +4,5 @@ module.exports = {
   githubClientId: 'a74612e3c105e9180c6d',
   githubSecret: '2c34cdeadd8f8fec4aac19c65c91ddc18f4a91ee',
   redisHost: '127.0.0.1',
-  redisPort: 6379,
-  port: 5000
+  redisPort: 6379
 };
